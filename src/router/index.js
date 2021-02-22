476869eb4a713d97049c369f9d7093529d61401b
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-02-22 14:31:03 +0100

    added Screens for routes [Home, Blog, Contact]

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import HomeScreen from '../screens/HomeScreen';
+import BlogScreen from '../screens/BlogScreen';
+import ContactScreen from '../screens/ContactScreen';
+
 const routes = [
-  /*
   {
     path: "/",
     name: "Home",
-    component: Home
-  },*/
+    component: HomeScreen,
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: HomeScreen,
+  },
+  {
+    path: "/blog",
+    name: "Blog",
+    component: BlogScreen,
+  },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: ContactScreen,
+  },
 ];
 
 const router = createRouter({

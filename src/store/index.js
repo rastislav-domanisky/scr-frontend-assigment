0d6144c1b2d01d90c67451f7e3636361f8d86b4e
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-02-22 12:08:41 +0100

    clean up defaults

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,18 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    articles: [
+      {
+        title: "Article Name",
+        category: "LIFESTYLE",
+        imgURL: "",
+        text: "fdsjksdfps,dfs dkfsúpf úsdfp dsfldäsl äňs.df.daä.l sfd,s"
+      }
+    ],
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {},
 });

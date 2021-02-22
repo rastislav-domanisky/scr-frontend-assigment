9970d852aabd599c4e4c7b158503e993ce8afae9
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-02-22 14:18:29 +0100

    installed FIrebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,20 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0hnXJ50SdWltwH0Swo1q8wSSN_motnoM",
+  authDomain: "scr-frontend-assigment.firebaseapp.com",
+  projectId: "scr-frontend-assigment",
+  storageBucket: "scr-frontend-assigment.appspot.com",
+  messagingSenderId: "369853383012",
+  appId: "1:369853383012:web:c18ef4c1cf25f076adfa71",
+  measurementId: "G-33T523FNP6"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 createApp(App)
   .use(store)
   .use(router)

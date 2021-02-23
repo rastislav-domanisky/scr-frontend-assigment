40b9cd1cf3182981b0ac8061fae57638ef59b3a8
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-02-23 17:22:44 +0100

    created ArticleScreen

---
+++ src/screens/ArticleScreen.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ArticleScreen"></div>
+</template>
+
+<script>
+export default {
+  name: "ArticleScreen",
+};
+</script>
+
+<style lang="scss" scoped>
+.ArticleScreen {
+  background: #fff;
+  width: 100%;
+  min-height: calc(100vh - 60px - 100px);
+  box-sizing: border-box;
+}
+</style>

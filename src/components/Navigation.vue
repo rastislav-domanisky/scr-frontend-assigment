1335f0183600375a30e9d2e7a664468f5146948e
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-02-23 13:27:26 +0100

    added Router-Link to logo

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -14,7 +14,9 @@
         d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
       />
     </svg>
-    <img :src="logoImg" alt="Logo" class="logo" />
+    <router-link :to="'/'">
+      <img :src="logoImg" alt="Logo" class="logo" />
+    </router-link>
     <NavItems :links="links" />
     <NavItemsMobile :links="links" v-show="isMenuOpened" />
   </nav>
